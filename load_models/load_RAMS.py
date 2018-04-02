b1f8f3fab6c5ae8e48db2750a4a5762120737b39
Max Heikenfeld <maxheikenfeld@web.de>
2018-04-02 05:27:54 +0100

    changed load function for rams from loadramscube to load

--- load_models/load_RAMS.py
+++ load_models/load_RAMS.py
@@ -1,4 +1,4 @@
-from ramscube import loadramscube
+from ramscube import load
 def load_RAMS(filenames,variable):
-    cube_out=loadramscube(filenames,variable,add_coordinates='xy')
+    cube_out=load(filenames,variable,add_coordinates='xy')
     return cube_out                    
