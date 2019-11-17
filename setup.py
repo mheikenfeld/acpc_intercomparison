6a209b34504eac28f4925999b19b40f063ad6e94
Max Heikenfeld <maxheikenfeld@web.de>
2019-11-17 19:40:24 +0100

    created installable package structure and added necessary files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='acpc_intercomparison',
+      version='1.0',
+      description='ACPC deep convection intercomparison setup',
+      url='http://github.com/mheikenfeld/acpc_intercomparison',
+      author='Max Heikenfeld',
+      author_email='max.heikenfeld@physics.ox.ac.uk',
+      license='GNU',
+      packages=['acpc_intercomparison','acpc_intercomparison.load_models','acpc_intercomparison.plot_functions'],
+      install_requires=[],
+      zip_safe=False)
